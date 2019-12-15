bdf6ef310fe4c31de0cae18ffb8a2970244b889e
beaubouchard <beau@beaubouchard.com>
2019-12-14 23:19:20 -0500

    feat: added post for slack

--- src/app.js
+++ src/app.js
@@ -31,44 +31,59 @@ app.use((req, res, next) => {
 
 logger.info('turning on app...');
 
-  /**
-   * @param {Request} req - Express request object
-   * @param {Response} res - Express response object
-   * @param {Next} next - Express Next object
-   */
-  app.get('/', (req, res, next) => {
-    requestsCount++;
-    logger.info(`/ request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
-    weather.getSingle(req).then((response) => {
-      res.status(200).send({ response_type: 'in_channel', text: response });
-      logger.debug(response);
-    }).catch((error) => {
-      this.sendMessage(message.channel, `${error}`);
-      res.status(400).send({ response_type: 'in_channel', text: response });
-    });
-
-
+/**
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.get('/', (req, res, next) => {
+  requestsCount++;
+  logger.info(`/ request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
+  weather.getSingle(req).then((response) => {
+    res.status(200).send({ response_type: 'in_channel', text: response });
+    logger.debug(response);
+  }).catch((error) => {
+    this.sendMessage(message.channel, `${error}`);
+    res.status(400).send({ response_type: 'in_channel', text: response });
   });
+});
 
-  /**
-   * @param {Request} req - Express request object
-   * @param {Response} res - Express response object
-   * @param {Next} next - Express Next object
-   */
-  app.get('/health', (req, res, next) => {
-    requestsCount++;
-    const time = process.uptime();
-    const uptime = format.toDDHHMMSS(time + '');
-    logger.info(`/health request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
-    res.status(200).send({ data: {uptime: uptime, version: pkjson.version, requests: requestsCount} });
+/**
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.post('/', (req, res, next) => {
+  requestsCount++;
+  logger.info(`/ request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
+  weather.getSingle(req).then((response) => {
+    res.status(200).send({ response_type: 'in_channel', text: response });
+    logger.debug(response);
+  }).catch((error) => {
+    this.sendMessage(message.channel, `${error}`);
+    res.status(400).send({ response_type: 'in_channel', text: response });
   });
+});
 
-  // heroku dynamically assigns your app a port, so you can't set the port to a fixed number.
-  var server = app.listen(process.env.PORT || 5000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+/**
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.get('/health', (req, res, next) => {
+  requestsCount++;
+  const time = process.uptime();
+  const uptime = format.toDDHHMMSS(time + '');
+  logger.info(`/health request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
+  res.status(200).send({ data: {uptime: uptime, version: pkjson.version, requests: requestsCount} });
+});
 
-    console.log('App listening at http://%s:%s', host, port);
-  });
+// heroku dynamically assigns your app a port, so you can't set the port to a fixed number.
+var server = app.listen(process.env.PORT || 5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-  module.exports = server;
+  console.log('App listening at http://%s:%s', host, port);
+});
+
+module.exports = server;
