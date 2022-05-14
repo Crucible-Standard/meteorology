1ab9527f7987ff3d8076ccfbf375d936afdb8add
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2022-05-14 02:29:21 -0400

    chore: update the entire packages (#100)
    * chore: update the entire packages
    
    * Rename test/app.test.js to tests/app.test.js
    
    * feat: adds the new github action, a lot of different items

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const helmet = require('helmet');
-const logger = require('@wh-iterabb-it/sst').logger;
-const format = require('@wh-iterabb-it/sst').format;
-const pkjson = require('../package.json');
-const weather = require('./models/weather');
+const { logger, format } = require("sst");
+
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const pkjson = require("../package.json");
+const weather = require("./models/weather");
 
 const app = express();
 
@@ -24,59 +24,81 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
  * @param {Next} next - Express Next object
  */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, Origin, X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, Origin, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "GET");
   next();
 });
 
-logger.info('turning on app...');
+logger.info("turning on app...");
 
 /**
  * @param {Request} req - Express request object
  * @param {Response} res - Express response object
- * @param {Next} next - Express Next object
  */
-app.get('/', (req, res, next) => {
+app.get("/", (req, res) => {
   requestsCount++;
-  logger.info(`/ request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
-  weather.getSingle(req).then((response) => {
-    res.status(200).send({ response_type: 'in_channel', text: response });
-    logger.debug(response);
-  }).catch((error) => {
-    this.sendMessage(message.channel, `${error}`);
-    res.status(400).send({ response_type: 'in_channel', text: response });
-  });
+  logger.info(
+    `/ request from ${
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.ip
+    }`
+  );
+  weather
+    .getSingle(req)
+    .then((response) => {
+      res.status(200).send({ response_type: "in_channel", text: response });
+      logger.debug(response);
+    })
+    .catch((error) => {
+      res.status(400).send({ response_type: "in_channel", text: error });
+    });
 });
 
 /**
  * @param {Request} req - Express request object
  * @param {Response} res - Express response object
- * @param {Next} next - Express Next object
  */
-app.post('/', (req, res, next) => {
+app.post("/", (req, res) => {
   requestsCount++;
-  logger.info(`/ request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
-  weather.getSingle(req).then((response) => {
-    res.status(200).send({ response_type: 'in_channel', text: response });
-    logger.debug(response);
-  }).catch((error) => {
-    this.sendMessage(message.channel, `${error}`);
-    res.status(400).send({ response_type: 'in_channel', text: response });
-  });
+  logger.info(
+    `/ request from ${
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.ip
+    }`
+  );
+  weather
+    .getSingle(req)
+    .then((response) => {
+      res.status(200).send({ response_type: "in_channel", text: response });
+      logger.debug(response);
+    })
+    .catch((error) => {
+      res.status(400).send({ response_type: "in_channel", text: error });
+    });
 });
 
 /**
  * @param {Request} req - Express request object
  * @param {Response} res - Express response object
- * @param {Next} next - Express Next object
  */
-app.get('/health', (req, res, next) => {
+app.get("/health", (req, res) => {
   requestsCount++;
   const time = process.uptime();
-  const uptime = format.toDDHHMMSS(time + '');
-  logger.info(`/health request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip}`);
-  res.status(200).send({ data: {uptime: uptime, version: pkjson.version, requests: requestsCount} });
+  const uptime = format.toDDHHMMSS(time + "");
+  logger.info(
+    `/health request from ${
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.ip
+    }`
+  );
+  res.status(200).send({
+    data: {
+      uptime: uptime,
+      version: pkjson.version,
+      requests: requestsCount,
+    },
+  });
 });
 
 // heroku dynamically assigns your app a port, so you can't set the port to a fixed number.
@@ -84,7 +106,7 @@ var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('App listening at http://%s:%s', host, port);
+  console.log("App listening at http://%s:%s", host, port);
 });
 
 module.exports = server;
