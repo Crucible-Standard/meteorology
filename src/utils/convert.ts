c215ce3790814a9e5d21572c15a2c617dba84086
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-05-24 13:42:09 -0400

    fix: tests (#283)

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -12,19 +12,6 @@ function kelvinToFahrenheit(kelvin: number, digit = 3): number {
 }
 
 /**
- * celsiusToFahrenheit
- * @param {int} celsius - temperature in celsius
- * @param {number} digit - default:3, the number of significant digits
- * @return {number} - temperature in fahrenheit
- **/
-function celsiusToFahrenheit(celsius: number, digit = 3): number {
-  return (
-    Math.round(((celsius * 9) / 5 + 32) * Math.pow(10, digit)) /
-    Math.pow(10, digit)
-  );
-}
-
-/**
  * kelvinToCelsius
  * @param {number} kelvin - temperature in kelvin
  * @param {number} digit - default:3, the number of significant digits
@@ -36,22 +23,4 @@ function kelvinToCelsius(kelvin: number, digit = 3): number {
   );
 }
 
-/**
- * fahrenheitToCelsius
- * @param {number} fahrenheit - temperature in fahrenheit
- * @param {number} digit - default:3, the number of significant digits
- * @return {number} - temperature in celsius
- **/
-function fahrenheitToCelsius(fahrenheit: number, digit = 3): number {
-  return (
-    Math.round((((fahrenheit * 1 - 32) * 5) / 9) * Math.pow(10, digit)) /
-    Math.pow(10, digit)
-  );
-}
-
-export {
-  kelvinToFahrenheit,
-  kelvinToCelsius,
-  fahrenheitToCelsius,
-  celsiusToFahrenheit,
-};
+export { kelvinToFahrenheit, kelvinToCelsius };
--- tests/utils/convert.test.ts
+++ tests/utils/convert.test.ts
@@ -1,9 +1,7 @@
 import {
   kelvinToFahrenheit,
   kelvinToCelsius,
-  fahrenheitToCelsius,
-  celsiusToFahrenheit,
-} from '../../lib/utils/convert';
+} from '../../src/utils/convert';
 
 const freezingKelvin = 273.15;
 const boilingKelvin = 373.15;
@@ -23,16 +21,6 @@ describe('Convert', () => {
 			expect(result).toEqual(boilingFahrenheit);
 		});
 	});
-	describe('celsiusToFahrenheit - Unit Tests', () => {
-		it(`should give Fahrenheit ${freezingFahrenheit} for freezing Celsius input at ${freezingCelsius}`, () => {
-			const result = celsiusToFahrenheit(freezingCelsius);
-			expect(result).toEqual(freezingFahrenheit);
-		});
-		it(`should give Fahrenheit ${boilingFahrenheit} for boiling Celsius input at ${boilingCelsius}`, () => {
-			const result = celsiusToFahrenheit(boilingCelsius);
-			expect(result).toEqual(boilingFahrenheit);
-		});
-	});
 	describe('kelvinToCelsius - Unit Tests', () => {
 		it(`should give Celsius ${freezingCelsius} for freezing kelvin input at ${freezingKelvin}`, () => {
 			const result = kelvinToCelsius(freezingKelvin);
@@ -41,16 +29,6 @@ describe('Convert', () => {
 		it(`should give Celsius ${boilingCelsius} for boiling kelvin input at ${boilingKelvin}`, () => {
 			const result = kelvinToCelsius(boilingKelvin);
 			expect(result).toEqual(boilingCelsius);
-		});
-	});
-	describe('fahrenheitToCelsius - Unit Tests', () => {
-		it(`should give Celsius ${freezingCelsius} for freezing Fahrenheit input at ${freezingFahrenheit}`, () => {
-			const result = fahrenheitToCelsius(freezingCelsius);
-			expect(result).toEqual(freezingFahrenheit);
-		});
-		it(`should give Celsius ${boilingCelsius} for boiling Fahrenheit input at ${boilingFahrenheit}`, () => {
-			const result = fahrenheitToCelsius(boilingCelsius);
-			expect(result).toEqual(boilingFahrenheit);
 		});
 	});
 });
